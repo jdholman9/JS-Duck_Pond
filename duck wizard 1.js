c7366aac478d108f85f4241ce491d34b795f86d1
Jacob Holman <jdholman9@gmail.com>
2020-08-13 15:27:32 -0700

    Clean up

--- duck wizard 1.js
+++ duck wizard 1.js
@@ -1,25 +1,22 @@
-﻿var dirc; // Cannon angle
+var dirc; // Cannon angle
 var rl = -1; // Used to determine rotation direction
 var ran = [5, 35, 60]; // attack ranges
 var wbuff = 5; // Desired wall buffer
-var chg = 70; // Health level cutoff
-
-
-
+var rota = -1; // rotation of scanner
 
 /**
  * attack function:  what to do when scan is picked up
  */
 function attack() {
   cannon(dirc, scan(dirc));
-  if(scan(dirc,20) < ran[0]){
+  if(scan(dirc) < ran[0]){
     swim(dirc + 180);
   }else if(scan(dirc) < ran[1]) {
     swim(dirc + rl*90);
   } else {
-    swim(dirc);
+    swim(dirc, 100);
   }
-  dirc = dirc - 30;
+  dirc = dirc - rota*35;
 }
 
 
@@ -48,7 +45,6 @@ function move() {
 
 
 
-
 dirc = Math.random() * 360;
 swim(dirc);
 if(Math.random() < 0.5){
@@ -57,13 +53,9 @@ if(Math.random() < 0.5){
 
 
 while(true){
-  while (health() > chg) {
-    move();
-    if (scan(dirc) <= ran[2]) {
-      attack();
-    }
-    dirc = dirc + 5;
+  move();
+  if (scan(dirc) <= ran[2]) {
+    attack();
   }
-  ran = [15, 40, 70];
-  chg = 0;
+  dirc = dirc + rota*5;
 }
