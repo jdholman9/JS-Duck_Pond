ace100addcb38692ae1a9d89741d011725c83ed0
Jacob Holman <jdholman9@gmail.com>
2020-08-13 15:17:26 -0700

    Update leech duck.js

--- leech duck.js
+++ leech duck.js
@@ -1,62 +1,16 @@
-﻿var dirc;
-var rl = -1;
-var wbuff = 2;
+var dirc = 0;
+var dist;
+var rota = -1;
 
-
-
-
-/**
- * Describe this function...
- */
-function attackRam() {
-  cannon(dirc, scan(dirc));
-  if(scan(dirc,20) < 10){
-    swim(dirc);
-  }else if(scan(dirc) < 40) {
-    swim(dirc);
-  } else {
-    swim(dirc);
+swim(0,100);
+while(true){
+  dist = scan(dirc);
+  if(dist <= 70){
+    swim(dirc, 100);
+    if(dist < 30){
+      cannon(dirc, dist);
+    }
+    dirc = dirc - rota*35;
   }
-  dirc = dirc + 35;
-}
-
-
-function walls() {
-  if(loc_y() < wbuff){
-    swim(Math.random()*180);
-    rl = -1*rl;
-  }
-  if(loc_x() > 100 - wbuff){
-    swim(Math.random()*180 + 90);
-    rl = -1*rl;
-  }
-  if(loc_y() > 100 - wbuff){
-    swim(Math.random()*180 + 180);
-    rl = -1*rl;
-  }
-  if(loc_x() < wbuff){
-    swim(Math.random()*180 + 270);
-    rl = -1*rl;
-  }
-}
-
-
-
-
-dirc = Math.random() * 360;
-swim(dirc);
-if(Math.random() < 0.5){
-  rl = 1;
-}
-
-
-while (true) {
-  walls();
-  if (scan(dirc) <= 20){
-    attackRam();
-  } else if(scan(dirc) < 100){
-    swim(dirc);
-    dirc = dirc + 50;
-  }
-  dirc = dirc - 5;
+  dirc = dirc + rota*5;
 }
