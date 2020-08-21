97f40618a1ecd62679846808d71dfc0d31415ed0
Jacob Holman <jdholman9@gmail.com>
2020-08-21 13:59:34 -0700

    Create Circler.js

---
+++ Circler.js
@@ -0,0 +1,52 @@
+
+
+var wbuff = 15;
+var swm_spd = 100;
+var dirc = 0;
+var dist;
+
+function move2(sm_dirc){
+  swim(sm_dirc, swm_spd);
+    dist = scan(dirc);
+    if(dist <= 70){
+      cannon(dirc, dist);
+      dirc = dirc - 35;
+    }
+    if(dirc % 360 == (sm_dirc + 190) % 360){
+      dirc = sm_dirc - 10;
+    }
+    dirc += 5;
+}
+
+
+while(true){
+  
+  while(getX() > wbuff){
+    move2(180);
+    if(speed() == 0){
+      break;
+    }
+  }
+  
+  while(getY() > wbuff){
+    move2(270);
+    if(speed() == 0){
+      break;
+    }
+  }
+  
+  while(getX() < 100 - wbuff){
+    move2(0);
+    if(speed() == 0){
+      break;
+    }
+  }
+  
+  while(getY() < 100 - wbuff){
+    move2(90);
+    if(speed() == 0){
+      break;
+    }
+  }
+  
+}
