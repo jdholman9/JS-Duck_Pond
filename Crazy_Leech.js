d728b0407bf1ad38a9c415341da268fa301b09cb
Jacob Holman <jdholman9@gmail.com>
2020-11-24 14:02:04 -0800

    Update and rename Crazy_Leach.js to Crazy_Leech.js

---
+++ Crazy_Leech.js
@@ -0,0 +1,27 @@
+var dirc, dist, rota, swm_spd;
+
+dirc = Math.random()*360;
+rota = 1;
+swm_spd = 100;
+
+swim(dirc, swm_spd);
+while(true){
+  dist = scan(dirc);
+  while(dist <= 70){
+    swim(dirc, swm_spd);
+    cannon(dirc, dist);
+    dist = scan(dirc)
+    if(speed() == 0 & dist > 20){
+      dirc = dirc + 180;
+    }
+  }
+  if(scan(dirc - rota*5) <= 70){
+    dirc = dirc - rota*5;
+  } else {
+    dirc = dirc + rota*5;
+  }
+  
+  if(speed() == 0 & dist > 20){
+    swim(Math.random()*360, swm_spd)
+  }
+}
